861bafd649168e8f60b0a5e9db695ae66a892669
TristanHoff <tristanhoff@hotmail.com>
2017-02-23 13:50:55 +0100

    ^_^
    Reserverings functie

--- reserveren.php
+++ reserveren.php
@@ -5,5 +5,15 @@
     </head>
     <body>
 <?php include 'build/navbar.php'?>
+
+	<div id="resmio-de-havenkom">
+	</div> 
+	<script>(function(d, s) {
+	var js, rjs = d.getElementsByTagName(s)[0];
+	js = d.createElement(s);
+	js.src = "//static.resmio.com/static/nl/widget.js#id=de-havenkom&amp;width=275px&amp;height=400px"; 
+	rjs.parentNode.insertBefore(js, rjs);
+	}(document, "script")); </script>
+	
     </body>
 </html>
